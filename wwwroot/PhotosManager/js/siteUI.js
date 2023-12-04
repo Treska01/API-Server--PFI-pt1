2ef8853e97cd56a35043f20e2d8cb8ae859aacfc
Treska01 <treska01@hotmail.com>
2023-12-04 15:03:33 -0500

    first chunk

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -15,6 +15,7 @@ function restoreContentScrollPosition() {
     $("#content")[0].scrollTop = contentScrollPosition;
 }
 function updateHeader() {
+    $("#header").empty();
     //todo
 }
 function renderAbout() {
@@ -40,4 +41,92 @@ function renderAbout() {
                 </p>
             </div>
         `))
+}
+function renderCreateProfil() {
+    noTimeout(); // ne pas limiter le temps d’inactivité
+    eraseContent(); // effacer le conteneur #content
+    updateHeader("Inscription", "createProfil"); // mettre à jour l’entête et menu
+    $("#newPhotoCmd").hide(); // camouffler l’icone de commande d’ajout de photo
+    $("#content").append(`
+        <form class="form" id="createProfilForm"'>
+            <fieldset>
+                <legend>Adresse ce courriel</legend>
+                <input type="email"
+                        class="form-control Email"
+                        name="Email"
+                        id="Email"
+                        placeholder="Courriel"
+                        required
+                        RequireMessage = 'Veuillez entrer votre courriel'
+                        InvalidMessage = 'Courriel invalide'
+                        CustomErrorMessage ="Ce courriel est déjà utilisé"/>
+                <input class="form-control MatchedInput"
+                        type="text" matchedInputId="Email"
+                        name="matchedEmail"
+                        id="matchedEmail"
+                        placeholder="Vérification"
+                        required
+                        RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+                        InvalidMessage="Les courriels ne correspondent pas" />
+            </fieldset>
+            <fieldset>
+                <legend>Mot de passe</legend>
+                <input type="password"
+                        class="form-control"
+                        name="Password"
+                        id="Password"
+                        placeholder="Mot de passe"
+                        required
+                        RequireMessage = 'Veuillez entrer un mot de passe'
+                        InvalidMessage = 'Mot de passe trop court'/>
+                <input class="form-control MatchedInput"
+                        type="password"
+                        matchedInputId="Password"
+                        name="matchedPassword"
+                        id="matchedPassword"
+                        placeholder="Vérification"
+                        required
+                        InvalidMessage="Ne correspond pas au mot de passe" />
+            </fieldset>
+            <fieldset>
+                <legend>Nom</legend>
+                <input type="text"
+                        class="form-control Alpha"
+                        name="Name"
+                        id="Name"
+                        placeholder="Nom"
+                        required
+                        RequireMessage = 'Veuillez entrer votre nom'
+                        InvalidMessage = 'Nom invalide'/>
+            </fieldset>
+            <fieldset>
+                <legend>Avatar</legend>
+                <div class='imageUploader'
+                    newImage='true'
+                    controlId='Avatar'
+                    imageSrc='images/no-avatar.png'
+                    waitingImage="images/Loading_icon.gif">
+                </div>
+            </fieldset>
+            <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+        </form>
+        <div class="cancel">
+            <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+        </div>
+    `);
+    $('#loginCmd').on('click', renderLoginForm); // call back sur clic
+    initFormValidation();
+    initImageUploaders();
+    $('#abortCmd').on('click', renderLoginForm); // call back sur clic
+    // ajouter le mécanisme de vérification de doublon de courriel
+    addConflictValidation(API.checkConflictURL(), 'Email', 'saveUser');
+    // call back la soumission du formulaire
+    $('#createProfilForm').on("submit", function (event) {
+        let profil = getFormData($('#createProfilForm'));
+        delete profil.matchedPassword;
+        delete profil.matchedEmail;
+        event.preventDefault();// empêcher le fureteur de soumettre une requête de soumission
+        showWaitingGif(); // afficher GIF d’attente
+        createProfil(profil); // commander la création au service API
+    });
 }
