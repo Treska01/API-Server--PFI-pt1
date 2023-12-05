588cfc5381f6920e05dde570f3aee1f609cc220e
Treska01 <treska01@hotmail.com>
2023-12-04 23:22:39 -0500

    second chunk

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -14,9 +14,151 @@ function saveContentScrollPosition() {
 function restoreContentScrollPosition() {
     $("#content")[0].scrollTop = contentScrollPosition;
 }
-function updateHeader() {
+function updateHeader(texte, cmd) {
     $("#header").empty();
-    //todo
+    loggedUser = API.retrieveLoggedUser();
+
+    $("#header").append(`
+    <span title='${texte}' id='${cmd}Cmd'>
+        <img src="images/PhotoCloudLogo.png" class="appLogo">
+    </span>
+    <span class="viewTitle">${texte}
+    </span>
+    <div class="headerMenusContainer">
+        <span>&nbsp;</span> <!--filler-->
+    </div>
+    `);
+
+    if (cmd == "listPhotos") {
+        $(".viewtitle").append(`
+        <div class="cmdIcon fa fa-plus" id="newPhotoCmd" title="Ajouter une photo"></div>
+        `);
+    }
+    
+
+    if (loggedUser != null) {
+        $(".headerMenusContainer").append(`
+        <i title="Modifier votre profil">
+        <div class="UserAvatarSmall"
+            userid="${loggedUser.Id}"
+            id="editProfilCmd"
+            style="background-image:url('${loggedUser.Avatar}')"
+            title="${loggedUser.Name}"></div>
+        </i>
+        `);
+
+        if (cmd != "editProfil") {
+            $("#editProfilCmd").on('click', renderEditProfil);
+        }
+    }
+
+    $(".headerMenusContainer").append(`
+    <div class="dropdown ms-auto dropdownLayout">
+        <!-- Articles de menu -->
+        <div data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="cmdIcon fa fa-ellipsis-vertical"></i>
+        </div>
+        <div class="dropdown-menu noselect">
+            <!--dynamic menu-->
+        </div>
+    </div>
+    `);
+
+    if(loggedUser != null) {
+        if (loggedUser.Authorizations == { readAccess: 2, writeAccess: 2 }) {
+            $(".dropdown-menu").append(`
+            <span class="dropdown-item" id="manageUserCmd">
+                <i class="menuIcon fas fa-user-cog mx-2"></i>
+                Gestion des usagers
+            </span>
+            <div class="dropdown-divider"></div>
+            `);
+
+            if (cmd != "manageUser") {
+                $("#manageUserCmd").on('click', renderManageUser);
+            }
+        }
+
+        $(".dropdown-menu").append(`
+        <span class="dropdown-item" id="logoutCmd">
+            <i class="menuIcon fa fa-sign-out mx-2"></i>
+            Déconnexion
+        </span>
+        <span class="dropdown-item" id="editProfilCmd">
+            <i class="menuIcon fa fa-user-edit mx-2"></i>
+            Modifier votre profil </span>
+        <div class="dropdown-divider"></div>
+        <span class="dropdown-item" id="listPhotosCmd">
+            <i class="menuIcon fa fa-image mx-2"></i>
+            Liste des photos
+        </span>
+        <div class="dropdown-divider"></div>
+        <span class="dropdown-item" id="sortByDateCmd">
+            <i class="menuIcon fa fa-check mx-2"></i>
+            <i class="menuIcon fa fa-calendar mx-2"></i>
+            Photos par date de création
+        </span>
+        <span class="dropdown-item" id="sortByOwnersCmd">
+            <i class="menuIcon fa fa-fw mx-2"></i>
+            <i class="menuIcon fa fa-users mx-2"></i>
+            Photos par créateur
+        </span>
+        <span class="dropdown-item" id="sortByLikesCmd">
+            <i class="menuIcon fa fa-fw mx-2"></i>
+            <i class="menuIcon fa fa-user mx-2"></i>
+            Photos les plus aiméés </span>
+        <span class="dropdown-item" id="ownerOnlyCmd">
+            <i class="menuIcon fa fa-fw mx-2"></i>
+            <i class="menuIcon fa fa-user mx-2"></i>
+            Mes photos
+        </span>
+        `);
+
+        $("#logoutCmd").on('click', API.logout);
+
+        if (cmd != "editProfil") {
+            $("#editProfilCmd").on('click', renderEditProfil);
+        }
+
+        if(cmd != "listPhotos") {
+            $("#listPhotosCmd").on('click', renderListPhotos);
+        }
+
+        if (cmd != "sortByDate") {
+            $("#sortByDateCmd").on('click', renderSortByDate);
+        }
+
+        if(cmd != "sortByOwners") {
+            $("#sortByOwnersCmd").on('click', renderSortByOwners);
+        }
+
+        if (cmd != "sortByLikes") {
+            $("#sortByLikesCmd").on('click', renderSortByLikes);
+        }
+
+        if (cmd != "ownerOnly") {
+            $("#ownerOnlyCmd").on('click', renderOwnerOnly);
+        }
+    } else {
+        $(".dropdown-menu").append(`
+        <span class="dropdown-item" id="loginCmd">
+            <i class="menuIcon fa fa-sign-in mx-2"></i>
+            Connexion
+        </span>
+        `);
+
+        $("#loginCmd").on('click', renderLoginForm);
+    }
+
+    $(".dropdown-menu").append(`
+    <div class="dropdown-divider"></div>
+    <span class="dropdown-item" id="aboutCmd">
+        <i class="menuIcon fa fa-info-circle mx-2"></i>
+        À propos...
+    </span>
+    `);
+
+    $("#loginCmd").on('click', renderAbout);
 }
 function renderAbout() {
     timeout();
@@ -128,5 +270,143 @@ function renderCreateProfil() {
         event.preventDefault();// empêcher le fureteur de soumettre une requête de soumission
         showWaitingGif(); // afficher GIF d’attente
         createProfil(profil); // commander la création au service API
+    });
+}
+function renderLoginForm() {
+    noTimeout(); // ne pas limiter le temps d’inactivité
+    eraseContent(); // effacer le conteneur #content
+    updateHeader("Connexion", "login"); // mettre à jour l’entête et menu
+    $("#newPhotoCmd").hide(); // camouffler l’icone de commande d’ajout de photo
+    $("#content").append(`
+        <h3>${loginMessage}</h3>
+        <form class="form" id="loginForm">
+            <input type='email'
+                    name='Email'
+                    class="form-control"
+                    required
+                    RequireMessage = 'Veuillez entrer votre courriel'
+                    InvalidMessage = 'Courriel invalide'
+                    placeholder="adresse de courriel"
+                    value='${Email}'>
+            <span style='color:red'>${EmailError}</span>
+            <input type='password'
+                    name='Password'
+                    placeholder='Mot de passe'
+                    class="form-control"
+                    required
+                    RequireMessage = 'Veuillez entrer votre mot de passe'>
+            <span style='color:red'>${passwordError}</span>
+            <input type='submit'
+                    name='submit'
+                    value="Entrer"
+                    class="form-control btn-primary">
+        </form>
+        <div class="form">
+            <hr>
+            <button class="form-control btn-info" id="createProfilCmd">Nouveau compte</button>
+        </div>
+    `);
+    $('#createProfilCmd').on('click', renderCreateProfil); // call back sur clic
+    initFormValidation();
+    // call back la soumission du formulaire
+    $('#createProfilForm').on("submit", function (event) {
+        let profil = getFormData($('#loginForm'));
+        event.preventDefault();// empêcher le fureteur de soumettre une requête de soumission
+        showWaitingGif(); // afficher GIF d’attente
+        login(profil.Email, profil.Password); // commander la création au service API
+    });
+}
+function renderEditProfil() {
+    noTimeout(); // ne pas limiter le temps d’inactivité
+    eraseContent(); // effacer le conteneur #content
+    updateHeader("Profil", "editProfil"); // mettre à jour l’entête et menu
+    loggedUser = API.retrieveLoggedUser();
+    $("#content").append(`
+    <form class="form" id="editProfilForm"'>
+        <input type="hidden" name="Id" id="Id" value="${loggedUser.Id}"/>
+        <fieldset>
+            <legend>Adresse ce courriel</legend>
+            <input type="email"
+                    class="form-control Email"
+                    name="Email"
+                    id="Email"
+                    placeholder="Courriel"
+                    required
+                    RequireMessage = 'Veuillez entrer votre courriel'
+                    InvalidMessage = 'Courriel invalide'
+                    CustomErrorMessage ="Ce courriel est déjà utilisé" value="${loggedUser.Email}" >
+            <input class="form-control MatchedInput"
+                    type="text"
+                    matchedInputId="Email"
+                    name="matchedEmail"
+                    id="matchedEmail"
+                    placeholder="Vérification"
+                    required
+                    RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+                    InvalidMessage="Les courriels ne correspondent pas"
+                    value="${loggedUser.Email}" >
+        </fieldset>
+        <fieldset>
+            <legend>Mot de passe</legend>
+            <input type="password"
+                    class="form-control"
+                    name="Password"
+                    id="Password"
+                    placeholder="Mot de passe"
+                    InvalidMessage = 'Mot de passe trop court' >
+            <input class="form-control MatchedInput"
+                    type="password"
+                    matchedInputId="Password"
+                    name="matchedPassword"
+                    id="matchedPassword"
+                    placeholder="Vérification"
+                    InvalidMessage="Ne correspond pas au mot de passe" >
+        </fieldset>
+        <fieldset>
+            <legend>Nom</legend>
+            <input type="text"
+                    class="form-control Alpha"
+                    name="Name"
+                    id="Name"
+                    placeholder="Nom"
+                    required
+                    RequireMessage = 'Veuillez entrer votre nom'
+                    InvalidMessage = 'Nom invalide'
+                    value="${loggedUser.Name}" >
+        </fieldset>
+        <fieldset>
+            <legend>Avatar</legend>
+            <div class='imageUploader'
+                newImage='false'
+                controlId='Avatar'
+                imageSrc='${loggedUser.Avatar}'
+                waitingImage="images/Loading_icon.gif">
+            </div>
+        </fieldset>
+        <input type='submit'
+                name='submit'
+                id='saveUserCmd'
+                value="Enregistrer"
+                class="form-control btn-primary">
+    </form>
+    <div class="cancel">
+        <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
+    </div>
+    <div class="cancel"> <hr>
+        <a href="confirmDeleteProfil.php">
+            <button class="form-control btn-warning">Effacer le compte</button>
+        </a>
+    </div>
+    `);
+    $('#createProfilCmd').on('click', renderCreateProfil); // call back sur clic
+    initFormValidation();
+    // call back la soumission du formulaire
+    $('#createProfilForm').on("submit", function (event) {
+        let profil = getFormData($('#loginForm'));
+        delete profil.matchedPassword;
+        delete profil.matchedEmail;
+        event.preventDefault();// empêcher le fureteur de soumettre une requête de soumission
+        showWaitingGif(); // afficher GIF d’attente
+        modifyUserProfil(profil); // commander la modification au service API
     });
 }
